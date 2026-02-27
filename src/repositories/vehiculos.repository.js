9fcd8e60cdf172816017b11f197c5c9d45e1b37c
Leonardo Pereda <leonardopereda7@hotmail.com>
2026-02-27 12:45:05 -0600

    Vehiculos repository completo

---
+++ src/repositories/vehiculos.repository.js
@@ -0,0 +1,55 @@
+const { pool } = require('../db');
+
+class VehiculosRepository {
+
+    async getAll() {
+        const result = await pool.query(
+            'SELECT id, marca, modelo, anio, dueno, placas, motivo, estado, fecha_ingreso FROM vehiculos;'
+        );
+
+        return result.rows;
+    }
+
+    async getAllActive() {
+        const result = await pool.query(
+            'SELECT id, marca, modelo, anio, dueno, placas,motivo, estado, fecha_ingreso FROM vehiculos WHERE activo = true;'
+        );
+
+        return result.rows;
+    }
+
+    async getByPlacas(placas) {
+        const result = await pool.query(
+            'SELECT id, marca, modelo, anio, dueno, placas, motivo, estado, fecha_ingreso, fecha_salida FROM vehiculos WHERE placas = $1;', [placas]
+        );
+
+        return result.rows[0] || null;
+    }
+
+    async create(marca, modelo, anio, dueno, placas, motivo) {
+        const result = await pool.query(
+            'INSERT INTO vehiculos (marca, modelo, anio, dueno, placas, motivo) values ($1, $2, $3, $4, $5, $6) returning id, marca, modelo, anio, dueno, placas, motivo, estado, fecha_ingreso;', [marca, modelo, anio, dueno, placas, motivo]
+        );
+
+        return result.rows[0];
+    }
+
+    async update(data) {
+        const result = await pool.query(
+            'UPDATE vehiculos SET marca = coalesce($1, marca), modelo = coalesce($2, modelo), anio = coalesce($3, anio), placas = coalesce($4, placas), dueno = coalesce($5, dueno), estado = ($6, estado), motivo = coalesce($7, motivo), activo = ($8, activo) WHERE placas = $9 returning id, marca, modelo, anio, placas, dueno, estado, motivo, activo, fecha_ingreso;',
+            [data.marca ?? data.modelo ?? data.anio ?? data.placas ?? data.dueno ?? data.estado ?? data.motivo ?? data.activo]
+        );
+
+        return result.rows[0] || null;
+    }
+
+    async delete(placas) {
+        const result = await pool.query(
+            'DELETE FROM vehiculos WHERE placas = $1 returning id;', [placas]
+        );
+
+        return result.rows[0] || null;
+    }
+}
+
+module.exports = { VehiculosRepository }
