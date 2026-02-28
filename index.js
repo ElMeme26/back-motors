a7c33e11089a3ee38952882ca11396d5feb46fc6
Leonardo Pereda <leonardopereda7@hotmail.com>
2026-02-28 00:13:25 -0600

    levantamiento del servidor

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const { pool } = require('./src/db');
+const { sign, authMiddleware } = require('./src/auth');
+const { router: vehiculosRouter } = require('./src/routes/vehiculos.routes');
+const { router: usersRouter } = require('./src/routes/users.routes');
+const { errorHandler } = require('./src/middlewares/error.middleware');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const allowed = [
+    'http://localhost:3000',
+    'http://localhost:3001'
+];
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // Limite de 100 solicitudes por IP
+    message: 'Demasiadas solicitudes, por favor intente de nuevo más tarde.'
+});
+
+app.use(limiter);
+
+app.use(cors({
+    origin: function (origin, cb) {
+        if (!origin) {
+            return cb(null, true);
+        }
+
+        if (allowed.includes(origin)) {
+            return cb(null, true);
+        }
+        
+        return cb(new Error('CORS bloqueado' + origin));
+    }
+}));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('API OK');
+});
+
+app.use('/vehiculos', vehiculosRouter);
+
+app.use('/users', usersRouter);
+
+app.get('privado', authMiddleware, (req, res) => {
+    return res.json({
+        ok: true,
+        user: req.user
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+});
+
+app.get('/health', async (req, res) => {
+    try {
+        await pool.query('SELECT 1');
+        return res.json({ ok: true });
+    } catch (err) {
+        return res.status(500).json({ ok: false });
+    }
+});
+
+app.get('/health/db', async (req, res) => {
+    try {
+        const r = await pool.query('select 1 as ok');
+        return res.json({ok:true, db:r.rows[0].ok});
+    } catch (err) {
+        console.log('DB Error', err.message);
+        return res.status(500).json({ok:false, error:'DB no disponible'});
+    }
+})
+
+app.use(errorHandler);
